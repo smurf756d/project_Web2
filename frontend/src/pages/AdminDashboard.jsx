ebd9046fe948b5b55b9454a4142d0aad1a8f75f0
leenbmn <leenbilal660@gmail.com>
2026-04-24 13:50:51 +0300

    add recent recipes panel

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -27,11 +27,15 @@ function AdminDashboard() {
   </div>
 </div>
 
-      <div style={{ marginTop: "30px" }}>
-        <h2>Recent Recipes</h2>
-        <p>No recipes yet</p>
-      </div>
-    </div>
+    <div style={{ marginTop: "30px" }}>
+  <h2>Recent Recipes</h2>
+  <div style={{ border: "1px solid white", padding: "15px", width: "300px" }}>
+    <p>No recipes yet</p>
+  </div>
+</div>
+
+  </div> 
+  
   );
 }
 
