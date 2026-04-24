61fa177041cecaffa8451c4d41ad3c3c773a0490
leenbmn <leenbilal660@gmail.com>
2026-04-24 14:37:32 +0300

    extract recent recipes into component

---
+++ frontend/src/pages/RecentRecipes.jsx
@@ -0,0 +1,8 @@
+import RecentRecipes from "./RecentRecipes";
+function RecentRecipes({ recipes }) {
+  return (
+  <RecentRecipes recipes={recentRecipes} />
+  );
+}
+
+export default RecentRecipes;
