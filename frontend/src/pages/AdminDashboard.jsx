9d70daf9bf5ae1768dd71cf76fe0690d130677dc
leenbmn <leenbilal660@gmail.com>
2026-04-24 15:31:48 +0300

    use recent recipes component in admin dashboard

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,3 +1,4 @@
+import RecentRecipes from "./RecentRecipes";
 import RecentUsers from "./RecentUsers";
 import StatsCards from "./StatsCards";
 import { recentUsers, recentRecipes } from "../data/adminData";
@@ -32,14 +33,8 @@ const stats = {
 
     <RecentUsers users={recentUsers} />
 
-      <div style={{ marginTop: "30px" }}>
-        <h2>Recent Recipes</h2>
-        <div style={{ border: "1px solid white", padding: "15px", width: "300px" }}>
-          {recentRecipes.map((recipe, index) => (
-  <p key={index}>{recipe}</p>
-))}
-        </div>
-      </div>
+  <RecentRecipes recipes={recentRecipes} />
+
     </div>
   );
 }
