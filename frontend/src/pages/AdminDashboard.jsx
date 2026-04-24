ba4919e4ad0c9543005ad39b971d1b3aa2b09bed
leenbmn <leenbilal660@gmail.com>
2026-04-24 13:44:48 +0300

    add recent users panel

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -20,10 +20,12 @@ function AdminDashboard() {
         </div>
       </div>
 
-      <div style={{ marginTop: "30px" }}>
-        <h2>Recent Users</h2>
-        <p>No users yet</p>
-      </div>
+     <div style={{ marginTop: "30px" }}>
+  <h2>Recent Users</h2>
+  <div style={{ border: "1px solid white", padding: "15px", width: "300px" }}>
+    <p>No users yet</p>
+  </div>
+</div>
 
       <div style={{ marginTop: "30px" }}>
         <h2>Recent Recipes</h2>
