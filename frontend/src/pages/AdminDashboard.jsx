616928f32edd37de5900b4a777b2e4a9d24f6588
leenbmn <leenbilal660@gmail.com>
2026-04-29 12:13:24 +0300

    add most used ingredients section

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -28,6 +28,40 @@ const stats = {
 </div>
     <StatsCards stats={stats} />
 
+<div className="card p-3 mb-4">
+  <h5 className="mb-3">Most Used Ingredients</h5>
+
+  <div className="mb-2">
+    <div className="d-flex justify-content-between">
+      <span>Chicken</span>
+      <span>130</span>
+    </div>
+    <div className="progress">
+      <div className="progress-bar bg-success" style={{ width: "85%" }}></div>
+    </div>
+  </div>
+
+  <div className="mb-2">
+    <div className="d-flex justify-content-between">
+      <span>Broccoli</span>
+      <span>110</span>
+    </div>
+    <div className="progress">
+      <div className="progress-bar bg-success" style={{ width: "70%" }}></div>
+    </div>
+  </div>
+
+  <div>
+    <div className="d-flex justify-content-between">
+      <span>Carrots</span>
+      <span>70</span>
+    </div>
+    <div className="progress">
+      <div className="progress-bar bg-success" style={{ width: "45%" }}></div>
+    </div>
+  </div>
+</div>
+
    <div className="card p-3 mb-4">
   <h5 className="mb-3">Recent Users</h5>
   <RecentUsers users={recentUsers} />
