64cafaa7d4fc26aa19142ad158c08c00ea76123b
leenbmn <leenbilal660@gmail.com>
2026-04-29 12:42:33 +0300

    add most liked recipes component

---
+++ frontend/src/pages/MostLikedRecipes.jsx
@@ -0,0 +1,21 @@
+function MostLikedRecipes() {
+  return (
+    <div className="card p-3 mt-4">
+      <h5 className="mb-3">Most Liked Recipes</h5>
+
+      <div className="list-group">
+        <div className="list-group-item d-flex justify-content-between">
+          <span>Grilled Chicken</span>
+          <span>225</span>
+        </div>
+
+        <div className="list-group-item d-flex justify-content-between">
+          <span>Veggie Stir Fry</span>
+          <span>180</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MostLikedRecipes;
