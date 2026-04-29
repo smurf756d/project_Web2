d918bc1e6a043fb492b659b0d22b313da6a209da
leenbmn <leenbilal660@gmail.com>
2026-04-29 12:06:21 +0300

    keep admin stats cards in one row

--- frontend/src/pages/StatsCards.jsx
+++ frontend/src/pages/StatsCards.jsx
@@ -1,21 +1,21 @@
 function StatsCards({ stats }) {
  return (
   <div className="row mb-4">
-    <div className="col-md-4">
+   <div className="col-4">
       <div className="card text-center p-3">
         <h5>Users</h5>
         <h3>{stats.users}</h3>
       </div>
     </div>
 
-    <div className="col-md-4">
+   <div className="col-4">
       <div className="card text-center p-3">
         <h5>Recipes</h5>
         <h3>{stats.recipes}</h3>
       </div>
     </div>
 
-    <div className="col-md-4">
+    <div className="col-4">
       <div className="card text-center p-3">
         <h5>Generated</h5>
         <h3>{stats.generated}</h3>
