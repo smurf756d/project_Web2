2cf31a14f9f5ad2878b7b8b6c0a5360d7d341768
noorsalahgit <s12429465@stu.najah.edu>
2026-05-06 23:22:06 +0300

    all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyRecipes from "./pages/MyRecipes";
+import Favorites from "./pages/Favorites";
 import HelpTips from "./pages/HelpTips";
 
 function App() {
-  return <HelpTips />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MyRecipes />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/help" element={<HelpTips />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
