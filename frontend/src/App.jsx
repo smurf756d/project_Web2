1027f3d9530f1b0b203d8749228f9aafbccad334
rama <ramaamjad2006@gmail.com>
2026-05-07 00:46:21 +0300

    save local changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UserDashboard from "./pages/UserDashboard";
-import GenerateRecipe from "./pages/GenerateRecipe";
-
-function BrowseRecipes() {
-  return <h2>Browse Recipes Page</h2>;
-}
-
-function MyRecipes() {
-  return <h2>My Recipes Page</h2>;
-}
-
-function Favorites() {
-  return <h2>Favorites Page</h2>;
-}
-
-function Help() {
-  return <h2>Help & Tips Page</h2>;
-}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="dashboard" element={<UserDashboard />} />
-          <Route path="generate-recipe" element={<GenerateRecipe />} />
-          <Route path="browse-recipes" element={<BrowseRecipes />} />
-          <Route path="my-recipes" element={<MyRecipes />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="help" element={<Help />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<UserDashboard />} />
+    </Routes>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
