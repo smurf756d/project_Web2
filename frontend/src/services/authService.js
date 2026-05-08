2da6d523e81c7e819bc3676790c8ff001b7946a1
rama <ramaamjad2006@gmail.com>
2026-05-09 02:02:59 +0300

    Save frontend dashboard

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,5 +2,8 @@ import axios from "axios";
 
 const API = "http://localhost:5000/api/auth";
 
-export const login = (data) => axios.post(`${API}/login`, data);
-export const register = (data) => axios.post(`${API}/register`, data);
+export const login = (data) =>
+  axios.post(`${API}/login`, data);
+
+export const register = (data) =>
+  axios.post(`${API}/register`, data);
